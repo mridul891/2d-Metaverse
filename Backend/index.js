905be5d76b8c5127da0f76a52afdc46f58f0b4c2
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-10-21 20:20:11 +0530

    Chat Connection Done Between the users

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express();
 const PORT = 3000
 const http = require('http')
-const { Server } = require(socket.io)
-const path = require('path')
+const { Server } = require('socket.io')
+const cors = require('cors')
 
 // Middlewares
 app.use(express.json());
@@ -11,10 +11,25 @@ app.use(cors())
 
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 
-io.on('connection', (socket) => console.log('A user connected', uuid()))
+io.on('connection', (socket) => {
+    console.log('User Connected With id', socket.id)
+
+    socket.on('message', (message) => socket.broadcast.emit("message", message))
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected");
+    });
+})
+
 
 
 server.listen(PORT, (req, res) => console.log(`Server is running at a ${PORT}`));
--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { io } from "socket.io-client";
+import "./App.css";
+import { useEffect, useMemo, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const socket = useMemo(() => io("http://localhost:3000"), []);
+  const [data, setData] = useState("");
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setData(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    socket.emit("message", data);
+    setData(" ");
+  };
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected", socket.id);
+    });
+
+    socket.on("message", (message) => {
+      console.log(message);
+    });
+  }, [socket]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input type="text" onChange={handleChange} value={data} />
+      <button onClick={handleSubmit}>submit</button>
+      {data}
+    </div>
+  );
 }
 
-export default App
+export default App;
